4f9a275c4608ff056860211dbdb25b35606111b4
adriana-diaz <122817007+adriana-diaz@users.noreply.github.com>
2023-02-05 16:21:52 -0600

    Create cheatsheet.ps1

---
+++ cheatsheet.ps1
@@ -0,0 +1,13 @@
+###################################################
+# Operators
+###################################################
+
+$a = 2                                                    # Basic variable assignment operator
+$a += 1                                                   # Incremental assignment operator
+$a -= 1                                                   # Decrement assignment operator
+
+$a -eq 0                                                  # Operador igual
+$a -ne 5                                                  # Operador distinto que
+$a -gt 2                                                  # Greater than comparison operator
+$a -lt 3                                                  # Less than comparison operator
+
